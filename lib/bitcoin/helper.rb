30399728e9d325a951e64bfd68b37b274a490a81
natsuokawai <kawai.natsuo@gmail.com>
2021-06-09 08:25:11 +0900

    Fix hash functions

--- lib/bitcoin/helper.rb
+++ lib/bitcoin/helper.rb
@@ -2,14 +2,6 @@ require 'digest'
 
 module Bitcoin
   module Helper
-    def self.hash256(str)
-      Digest::SHA256.hexdigest(Digest::SHA256.digest(str)).hex
-    end
-
-    def self.hash160(str)
-      Digest::RMD160.hexdigest(Digest::SHA256.digest(str)).hex
-    end
-
     def self.int_to_bytes(base_num, size, order)
       unless %i(big little).include?(order)
         raise ArgumentError, 'order must be either :little or :big'
@@ -27,6 +19,14 @@ module Bitcoin
 
       bytes.reverse! if order == :big
       (0...bytes.size).map { |i| bytes[i].ord << i * 8 }.sum
+    end
+
+    def self.hash256(str)
+      Digest::SHA256.digest(Digest::SHA256.digest(str))
+    end
+
+    def self.hash160(str)
+      Digest::RMD160.digest(Digest::SHA256.digest(str))
     end
 
     BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
--- spec/bitcoin/helper_spec.rb
+++ spec/bitcoin/helper_spec.rb
@@ -4,7 +4,7 @@ module Bitcoin
   describe Helper do
     describe 'self.hash256' do
       let(:str) { 'Programming Bitcoin!' }
-      let(:expected) { '969f6056aa26f7d2795fd013fe88868d09c9f6aed96965016e1936ae47060d48'.hex }
+      let(:expected) { Helper.int_to_bytes(0x969f6056aa26f7d2795fd013fe88868d09c9f6aed96965016e1936ae47060d48, 32, :big) }
       it { expect(Helper.hash256(str)).to eq expected }
     end
 
